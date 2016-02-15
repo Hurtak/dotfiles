b99e9d1780a9b461e41bd7c281040cc04e9bb10d
Petr Huřťák <petr.hurtak@gmail.com>
2016-02-15 10:31:01 +0100

    install.sh: fix wrong filenames

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ echo -e '\n' >> ~/.bash_aliases
 echo .bash_aliases >> ~/.bash_aliases
 
 echo -e '\n' >> ~/.gitconfig
-echo .bash_aliases >> ~/.gitconfig
+echo .gitconfig >> ~/.gitconfig
 
 echo -e '\n' >> ~/.bashrc
-echo .bash_aliases >> ~/.bashrc
+echo .bashrc >> ~/.bashrc
 
 source ~/.bashrc
