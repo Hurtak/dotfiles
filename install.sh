5b9a145d00bfb56ac3934d08ef26cbee61894815
Petr Huřťák <petr.hurtak@gmail.com>
2016-02-15 10:28:45 +0100

    add install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo -e '\n' >> ~/.bash_aliases
+echo .bash_aliases >> ~/.bash_aliases
+
+echo -e '\n' >> ~/.gitconfig
+echo .bash_aliases >> ~/.gitconfig
+
+echo -e '\n' >> ~/.bashrc
+echo .bash_aliases >> ~/.bashrc
+
+source ~/.bashrc
