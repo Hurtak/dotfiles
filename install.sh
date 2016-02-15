79e6cf48ca77e381f42f3f6d085722fd77149d43
Petr Huřťák <petr.hurtak@gmail.com>
2016-02-15 10:45:42 +0100

    install.sh: add #!/bin/bash, fix typos

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-echo 'starting instalation script'
+#!/bin/bash
+echo 'starting installation script'
 
 echo -e '\n' >> ~/.bash_aliases
 cat .bash_aliases >> ~/.bash_aliases
@@ -12,4 +13,4 @@ cat .bashrc >> ~/.bashrc
 echo 'reloading .bashrc'
 source ~/.bashrc
 
-echo 'instalation script finished'
+echo 'installation script finished'
