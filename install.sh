5ea20581f77df9e4005110edd8d26c62e6d37f60
Petr Huřťák <petr.hurtak@gmail.com>
2016-03-29 15:22:39 +0200

    install.sh: install script overwrites some files isntead of appending to them

--- install.sh
+++ install.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
+
 echo '1. starting installation script'
 
-echo -e '\n' >> ~/.bash_aliases
-cat .bash_aliases >> ~/.bash_aliases
-
-echo -e '\n' >> ~/.gitconfig
-cat .gitconfig >> ~/.gitconfig
-
+cat .bash_aliases > ~/.bash_aliases
+cat .gitconfig > ~/.gitconfig
+cat .vimrc > ~/.vimrc
 echo -e '\n' >> ~/.bashrc
 cat .bashrc >> ~/.bashrc
 
-echo -e '\n' >> ~/.vimrc
-cat .vimrc >> ~/.vimrc
-
 echo '2. reloading .bashrc'
+
 source ~/.bashrc
 
 echo '3. install packages into ~/bin directory'
+
 mkdir -p ~/bin
 mkdir -p ~/node_modules
 npm install --prefix ~/ tldr # better manual
