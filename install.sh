49002c15216459eb3eaf611b0ff85ae2aadf5fbd
Petr Huřťák <petr.hurtak@gmail.com>
2016-03-29 15:14:35 +0200

    install.sh: add -p into mkdir

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ echo '2. reloading .bashrc'
 source ~/.bashrc
 
 echo '3. install packages into ~/bin directory'
-mkdir ~/bin
-mkdir ~/node_modules
+mkdir -p ~/bin
+mkdir -p ~/node_modules
 npm install --prefix ~/ tldr # better manual
 npm install --prefix ~/ cloc # count lines of code
 ln -sf ~/node_modules/.bin/tldr ~/bin
