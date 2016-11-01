0bf75d7917fd259756fa2517acee41ed259193ec
Petr Huřťák <petr.hurtak@gmail.com>
2016-11-01 22:39:39 +0100

    install.sh: add comments

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash -e
 
 function write_or_append () {
+    # $1 copied config path
+    # $2 target config path
+    # $3 comment separator
+
     if [ -f $2 ]; then
+        # if file exists, add comment separator and append new config
         echo >> $2
         $3 >> $2
         echo >> $2
 
         cat $1 >> $2
     else
+        # if file does not exists, create it
         cat $1 > $2
     fi
 }
